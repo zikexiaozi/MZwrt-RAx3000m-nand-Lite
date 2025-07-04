9c9258eacf79a357d97bbd34d30a63adeeb47781
zikexiaozi <97644359@qq.com>
2025-07-04 10:17:46 +0800

    Update diy1.sh

--- DIY/diy1.sh
+++ DIY/diy1.sh
@@ -6,3 +6,4 @@
 
 ##添加自己的插件库
 echo -e "\nsrc-git mzwrt_package https://github.com/mzwrt/mzwrt_package_Lite.git" >> feeds.conf.default
+echo -e "\nsrc-git smpackage https://github.com/kenzok8/small-package" >> feeds.conf.default
